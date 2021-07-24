53d14e67353072f42d8c5b9a01d9bdb1d1f999c6
mselmank <m.selmankunstmann@gmail.com>
2021-07-24 13:07:52 -0400

    Creando simple user en MongoDB

--- backend/data.js
+++ backend/data.js
@@ -1,104 +1,124 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
-products:[
-    {
-        _id:'1',
-        name:'Overol piloto con reflectantes',
-        category:'security clothes',
-        image:'../images/p2.jpg',
-        price:8990,
-        countInStock:0,
-        brand:'generico',
-        rating:4.5,
-        numReviews:5,
-        description:'Excelente calidad'
+    users: [
+        {
 
-    },
-    {
-        _id:'2',
-        name:'Overol piloto sin reflectantes',
-        category:'security clothes',
-        image:'../images/p2.jpg',
-        price:9990,
-        countInStock:20,
-        brand:'generico',
-        rating:4.5,
-        numReviews:20,
-        description:'Excelente calidad'
+            name: 'Matias',
+            email: 'admin@example.com',
+            password: bcrypt.hashSync('1234', 8),
+            isAdmin: true,
 
-    },
+        },
+        {
+            name: 'Alfredo',
+            email: 'user@example.com',
+            password: bcrypt.hashSync('1234', 8),
+            isAdmin: false,
 
-    {
-        _id:'3',
-        name:'Overol piloto',
-        category:'security clothes',
-        image:'../images/p3.jpg',
-        price:7990,
-        countInStock:5,
-        brand:'generico',
-        rating:4.5,
-        numReviews:15,
-        description:'Buena calidad'
+        }
 
-    },
+    ],
+    products: [
+        {
+            _id: '1',
+            name: 'Overol piloto con reflectantes',
+            category: 'security clothes',
+            image: '../images/p2.jpg',
+            price: 8990,
+            countInStock: 5,
+            brand: 'generico',
+            rating: 4.5,
+            numReviews: 5,
+            description: 'Excelente calidad'
 
-    {
-        _id:'4',
-        name:'Overol de alta resistencia',
-        category:'security clothes',
-        image:'../images/p4.jpg',
-        price:5990,
-        countInStock:30,
-        brand:'generico',
-        rating:4.5,
-        numReviews:10,
-        description:'Excelente calidad'
+        },
+        {
+            _id: '2',
+            name: 'Overol piloto sin reflectantes',
+            category: 'security clothes',
+            image: '../images/p2.jpg',
+            price: 9990,
+            countInStock: 20,
+            brand: 'generico',
+            rating: 4.5,
+            numReviews: 20,
+            description: 'Excelente calidad'
 
-    },
+        },
 
-    {
-        _id:'5',
-        name:'Overol verde',
-        category:'security clothes',
-        image:'../images/p4.jpg',
-        price:6990,
-        countInStock:0,
-        brand:'generico',
-        rating:4.5,
-        numReviews:10,
-        description:'Excelente calidad'
+        {
+            _id: '3',
+            name: 'Overol piloto',
+            category: 'security clothes',
+            image: '../images/p3.jpg',
+            price: 7990,
+            countInStock: 40,
+            brand: 'generico',
+            rating: 4.5,
+            numReviews: 15,
+            description: 'Buena calidad'
 
-    },
+        },
 
-    {
-        _id:'6',
-        name:'Overol para construccion',
-        category:'security clothes',
-        image:'../images/p4.jpg',
-        price:2990,
-        countInStock:4,
-        brand:'generico',
-        rating:4.5,
-        numReviews:10,
-        description:'Excelente calidad'
+        {
+            _id: '4',
+            name: 'Overol de alta resistencia',
+            category: 'security clothes',
+            image: '../images/p4.jpg',
+            price: 5990,
+            countInStock: 0,
+            brand: 'generico',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Excelente calidad'
 
-    },
-    {
-        _id:'7',
-        name:'Overol de cocina',
-        category:'security clothes',
-        image:'../images/p4.jpg',
-        price:10990,
-        countInStock:0,
-        brand:'generico',
-        rating:3.5,
-        numReviews:56,
-        description:'Excelente '
+        },
 
-    },
+        {
+            _id: '5',
+            name: 'Overol verde',
+            category: 'security clothes',
+            image: '../images/p4.jpg',
+            price: 6990,
+            countInStock: 9,
+            brand: 'generico',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Excelente calidad'
+
+        },
+
+        {
+            _id: '6',
+            name: 'Overol para construccion',
+            category: 'security clothes',
+            image: '../images/p4.jpg',
+            price: 2990,
+            countInStock: 18,
+            brand: 'generico',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Excelente calidad'
+
+        },
+        {
+            _id: '7',
+            name: 'Overol de cocina',
+            category: 'security clothes',
+            image: '../images/p4.jpg',
+            price: 10990,
+            countInStock: 25,
+            brand: 'generico',
+            rating: 3.5,
+            numReviews: 56,
+            description: 'Excelente '
+
+        },
 
 
 
-],
+    ],
 
 
 };
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import userRouter from './routers/userRouters.js';
 
 
 const app = express ();
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/segurito',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 app.get('/api/products/:id',(req,res)=>{
     const product = data.products.find((x) => x._id === req.params.id);
@@ -18,9 +25,17 @@ app.get('/api/products/:id',(req,res)=>{
         res.send(data.products);
       });
 
+      app.use('/api/users/',userRouter);
+
+
       app.get('/', (req, res) => {
         res.send('Servidor listo');
       });
+      app.use((err, req, res, next) => {
+        res.status(500).send({ message: err.message });
+      });
+
+
 
 const port = process.env.PORT || 5000;
 app.listen(port,()=>{
